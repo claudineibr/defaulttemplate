e9786e526fcf05a5fe3874a52de5a909601c37ae
Claudinei Nascimento <claudinei@nascorp.com.br>
2019-02-15 18:19:29 -0200

    adjust call to cache

--- ProjetoPadraoNetCore.WebApi/Startup.cs
+++ ProjetoPadraoNetCore.WebApi/Startup.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using NascorpLib.Cache.Redis;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using ProjetoPadraoNetCore.ApplicationService;
 using ProjetoPadraoNetCore.Domain.IApplicationService;
@@ -14,6 +18,7 @@ using ProjetoPadraoNetCore.Repository;
 using ProjetoPadraoNetCore.Repository.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO.Compression;
 
 namespace ProjetoPadraoNetCore.WebApi
@@ -35,7 +40,6 @@ namespace ProjetoPadraoNetCore.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
             services.AddSwaggerGen(options =>
             {
@@ -59,7 +63,13 @@ namespace ProjetoPadraoNetCore.WebApi
                 });
             });
 
-            //services.AddDbContext<ProjetoPadraoNetCoreDBContext>(option => option.UseOracle(Config.ConnectionString));
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddConfiguration(Configuration.GetSection("Logging"));
+                loggingBuilder.AddConsole();
+                loggingBuilder.AddDebug();
+            });
+
             services.AddDbContext<ProjetoPadraoNetCoreDBContext>(option => option.UseMySql(Config.ConnectionString, mySqlOptions =>
             {
                 mySqlOptions.ServerVersion(new Version(5, 7, 17), ServerType.MySql); // replace with your Server Version and Type
@@ -77,12 +87,8 @@ namespace ProjetoPadraoNetCore.WebApi
                 options.Level = CompressionLevel.Fastest;
             });
 
-            //services.AddDistributedRedisCache(options =>
-            //{
-            //    options.Configuration = Config.CacheServer;
-            //    options.InstanceName = Config.CacheInstanceName;
-            //});
             services.AddScoped<DbContext>(sp => sp.GetService<ProjetoPadraoNetCoreDBContext>());
+            services.AddSingleton<CacheExchange>(provider => new CacheExchange(Config.CacheRedisConnection, Config.CacheRedisDatabase, Config.CacheInstanceName, Config.CacheExpireTime, Config.CacheRedisTimeout));
             services.AddTransient<IMeuServicoApplicationService, MeuServicoApplicationService>();
             services.AddTransient<IMeuServicoRepository, MeuServicoRepository>();
         }
@@ -90,13 +96,44 @@ namespace ProjetoPadraoNetCore.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            var defaultDateCulture = "pt-BR";
+            var ci = new CultureInfo(defaultDateCulture);
+            ci.NumberFormat.NumberDecimalSeparator = ",";
+            ci.NumberFormat.CurrencyDecimalSeparator = ",";
+
+            // Configure the Localization middleware
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(ci),
+                SupportedCultures = new List<CultureInfo> { ci },
+                SupportedUICultures = new List<CultureInfo> { ci }
+            });
+
             app.UseMvc();
-            app.UseSwagger()
-           .UseSwaggerUI(c =>
-           {
-               c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjetoPadraoNetCore API");
-           });
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger().UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjetoPadraoNetCore");
+            });
+
+            app.UseResponseCaching();
+            app.Use(async (context, next) =>
+            {
+                // For GetTypedHeaders, add: using Microsoft.AspNetCore.Http;
+                context.Response.GetTypedHeaders().CacheControl =
+                    new CacheControlHeaderValue()
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromHours(23)
+                    };
+                context.Response.Headers[HeaderNames.Vary] =
+                    new string[] { "Accept-Encoding" };
+
+                await next();
+            });
         }
     }
 }
